856d51a5105e8cc5c5d6ef7f9e97bf02a8970735
DamassoHD <41909390+DamassoHD@users.noreply.github.com>
2018-09-03 07:27:19 +0530

    Update wifi_modes.ino
    Try this without changing the pin_led first. Then you can meddle with the output pins,i.e. LEDs later.

--- wifi_modes/wifi_modes.ino
+++ wifi_modes/wifi_modes.ino
@@ -3,17 +3,17 @@
 
 ESP8266WebServer server;
 uint8_t pin_led = 16;
-char* ssid = "NETWORK_SSID";
-char* password = "13meetup37";
-char* mySsid = "ESP8266_SSID";
-
-IPAddress ip(192,168,11,4);
-IPAddress gateway(192,168,11,1);
+char* ssid = "NETWORK_SSID";      //Your WiFi hotspot name
+char* password = "13meetup37";.   //Your Wifi Hotspot asswordpno special characters or spaces 
+char* mySsid = "ESP8266_SSID";.   // Guest Hotspot name 
+//The following 3 lines are your nodemcu custom IP, subnet, gateway addresses
+IPAddress ip(192,168,11,4);.      
+IPAddress gateway(192,168,11,1);. 
 IPAddress subnet(255,255,255,0);
 
 void setup()
 {
-  pinMode(pin_led, OUTPUT);
+  pinMode(pin_led, OUTPUT);.      //Customizable output pin... Try using an external LED riggrd to the nodemcu. Note that other pins might have to be configured as well...
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid,password);
   Serial.begin(115200);
@@ -26,8 +26,8 @@ void setup()
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/",[](){server.send(200,"text/plain","Hello World!");});
-  server.on("/toggle",toggleLED);
+  server.on("/",[](){server.send(200,"text/plain","Hello World!");});. //Message to be didplayed in the IP address browser tab
+  server.on("/toggle",toggleLED);.    
   server.begin();
 
   WiFi.softAPConfig(ip, gateway, subnet);
